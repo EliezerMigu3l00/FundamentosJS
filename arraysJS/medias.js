4a89376c841b843b53fd6fd963f5d3339d7da791
Eliezer Miguel <99448887+EliezerMigu3l00@users.noreply.github.com>
2022-02-27 13:50:01 -0300

    inciando os estudos de arrays

---
+++ arraysJS/medias.js
@@ -0,0 +1,18 @@
+//Trecho de código para calcular médias sem arrays
+
+let nota1 = 10;
+let nota2 = 6.5;
+let nota3 = 8;
+let nota4 = 7.5;
+
+const medias = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(medias);
+
+//Trecho de código para calcular médias com arrays
+
+const notas = [10, 6.5, 8, 7.5]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media)
