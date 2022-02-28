255a5f2a98e881f936d7668e20691757660d1e25
Eliezer Miguel <99448887+EliezerMigu3l00@users.noreply.github.com>
2022-02-28 00:47:53 -0300

    atualizando e organizano diretorio

---
+++ arraysJS/atualizando-lista.js
@@ -0,0 +1,5 @@
+const listaDeChamadas = ['João', 'Juliana', 'Ana', 'Caio', 'Lara']
+
+listaDeChamadas.splice(1,2,`Rodrigo`)
+
+console.log(`Lista de chamada atualizada: ${listaDeChamadas}`)
---
+++ arraysJS/concatenacao-arrays.js
@@ -0,0 +1,6 @@
+const salaPython = ['Jose', 'Juliana', 'Marcos']
+const salaJavaScript = ['Marcela', 'João', 'Eliezer']
+
+const salasUnificadas = salaPython.concat(salaJavaScript)
+
+console.log(salasUnificadas)
